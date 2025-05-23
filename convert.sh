46f8e9eae96776dfb0dbda3bbaac9ff9ca55b65b
Om Raheja <om@omraheja.me>
2025-05-23 11:56:43 -0400

    feat(analytics): added ganalytics

--- convert.sh
+++ convert.sh
@@ -1,24 +1,27 @@
+#!/bin/sh
+
+GOOGLE_TAG="<!-- Google tag (gtag.js) --><script async src=\"https://www.googletagmanager.com/gtag/js?id=G-V361P3KSWH\"></script><script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-V361P3KSWH');</script>"
+
 for filename in *.md; do
     [ -e "$filename" ] || continue
     clean=$(basename "$filename" .md)
     if [ "$clean" = "index" ]; then
-        # Custom processing for index.md with title and meta description
         lowdown -s \
             --html-no-skiphtml \
             --html-no-escapehtml \
             --html-hardwrap \
             -mtitle="Om Raheja - Entrepreneur & Software Engineer" \
             -mcss=style.css \
-            -M htmlheader='<meta name="description" content="Om Raheja. Entrepreneur and software engineer. High School North Class of 2026. Creator of aquarc and codeabode.">' \
+            -M htmlheader='<meta name="description" content="Om Raheja. Entrepreneur and software engineer. High School North Class of 2026. Creator of aquarc and codeabode.">'"$GOOGLE_TAG" \
             "$filename" > "$clean.html"
     else
-        # Default processing for other pages
         lowdown -s \
             --html-no-skiphtml \
             --html-no-escapehtml \
             --html-hardwrap \
             -mtitle="$clean" \
             -mcss=style.css \
+            -M htmlheader="$GOOGLE_TAG" \
             "$filename" > "$clean.html"
     fi
 done
